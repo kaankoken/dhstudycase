9814d6e122313fc33fc611d1238dd5dbd40a297d
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-25 12:48:21 +0300

    Add initial commit for splash

--- lib/presentation/splash/splash_view.dart
+++ lib/presentation/splash/splash_view.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+class SplashView extends ConsumerWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    throw UnimplementedError();
+  }
+}

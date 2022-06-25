8279b6872bca8075d831c7f51c8eea5ffb229eea
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-25 15:50:55 +0300

    Update fake splash view completed

--- lib/presentation/splash/splash_view_model.dart
+++ lib/presentation/splash/splash_view_model.dart
@@ -1,5 +1,22 @@
 import 'package:delivery_hero_flutter_study_case/infra/core/base/base_view_model.dart';
+import 'package:delivery_hero_flutter_study_case/infra/core/extensions/dark_mode.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 class SplashViewModel extends BaseViewModel {
-  SplashViewModel(super.ref);
+  bool isDarkMode = WidgetsBinding.instance.isDarkMode;
+
+  SplashViewModel(super.ref) {
+    _init();
+  }
+
+  void _init() async {
+    // Fake app loading action
+    return Future.delayed(
+      const Duration(seconds: 2),
+      () async {
+        FlutterNativeSplash.remove();
+      },
+    );
+  }
 }

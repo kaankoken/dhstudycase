b2b77749cda660537264b92be337d61da512d206
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-25 15:50:00 +0300

    Add dimension & color added to app structure

---
+++ lib/infra/core/constants/app_dimensions.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: member-ordering
+
+class AppDimensions {
+  static const double padding5 = 5.0;
+  static const double padding8 = 8.0;
+  static const double padding12 = 12.0;
+  static const double padding16 = 16.0;
+  static const double padding20 = 20.0;
+  static const double padding25 = 25.0;
+  static const double padding64 = 64.0;
+}

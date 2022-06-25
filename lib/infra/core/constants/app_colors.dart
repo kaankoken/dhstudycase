a92144f037f6eec8a24cd087ddfd175c237ac211
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-25 15:50:00 +0300

    Add dimension & color added to app structure

---
+++ lib/infra/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:delivery_hero_flutter_study_case/infra/core/extensions/hex_to_flutter_color.dart';
+
+class AppColors {
+  static final RED = '#ff0000'.toColor();
+  static final WHITE = "#ffffff".toColor();
+}

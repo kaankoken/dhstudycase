75c2d89834a5adea37e578e869d5e031c93e97e3
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-26 16:14:28 +0300

    Add prod & dev environment added in flutter side

--- lib/infra/exports.dart
+++ lib/infra/exports.dart
@@ -1,9 +1,11 @@
 import 'package:delivery_hero_flutter_study_case/infra/core/localization/app_strings.dart';
 import 'package:delivery_hero_flutter_study_case/infra/core/routes/routes_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
 
 late AppStrings appStrings;
 late GoRouter route;
 final RouteManager routeManager = RouteManager.instance;
 final GlobalKey<ScaffoldMessengerState> scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+final DotEnv dotEnv = DotEnv();

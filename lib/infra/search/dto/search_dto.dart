c63946516b18fe8648704b2d670cf83da99cbd42
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-26 16:21:37 +0300

    Add dto models are created

---
+++ lib/infra/search/dto/search_dto.dart
@@ -0,0 +1,17 @@
+import 'package:delivery_hero_flutter_study_case/infra/search/dto/results_dto.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'search_dto.freezed.dart';
+part 'search_dto.g.dart';
+
+@Freezed()
+class SearchDto with _$SearchDto {
+  const factory SearchDto({
+    int? page,
+    @JsonKey(name: 'results') List<ResultsDto>? results,
+    @JsonKey(name: 'total_results') int? totalResults,
+    @JsonKey(name: 'total_pages') int? totalPages,
+  }) = _SearchDto;
+
+  factory SearchDto.fromJson(Map<String, dynamic> json) => _$SearchDtoFromJson(json);
+}

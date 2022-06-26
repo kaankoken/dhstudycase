f08084b9cf58177d103f9643dfc69112f9b36c09
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-26 22:24:14 +0300

    Add initial commit for search screen

---
+++ lib/domain/search/search_route.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:delivery_hero_flutter_study_case/domain/core/i_route.dart';
+import 'package:delivery_hero_flutter_study_case/infra/core/extensions/remove_slash.dart';
+import 'package:delivery_hero_flutter_study_case/infra/core/routes/app_routes.dart';
+import 'package:delivery_hero_flutter_study_case/presentation/search/search_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeRoute implements IRoute {
+  @override
+  GoRoute get route => GoRoute(
+        path: AppRoutes.SEARCH,
+        name: AppRoutes.SEARCH.removeSlash,
+        pageBuilder: (context, state) => Platform.isAndroid
+            ? MaterialPage(
+                child: const SearchView(),
+                arguments: state.extra,
+                fullscreenDialog: true,
+              )
+            : CupertinoPage(
+                child: const SearchView(),
+                arguments: state.extra,
+                fullscreenDialog: true,
+              ),
+      );
+}

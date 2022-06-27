44b32639d8b0e55140169540e64276e740a75bda
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-27 10:07:27 +0300

    Add search use case is created with usecase module

---
+++ lib/application/search/search_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:delivery_hero_flutter_study_case/application/search/interfaces/i_search_use_case.dart';
+import 'package:delivery_hero_flutter_study_case/domain/search/i_search_repository.dart';
+import 'package:delivery_hero_flutter_study_case/infra/core/modules/repository_modules.dart';
+import 'package:delivery_hero_flutter_study_case/presentation/core/ui_state.dart';
+import 'package:delivery_hero_flutter_study_case/domain/search/search_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SearchUseCase implements ISearchUseCase {
+  late final ISearchRepository _repository;
+
+  SearchUseCase(Ref ref) : _repository = ref.read(searchRepository);
+
+  @override
+  Future<UIState<String, SearchModel>> invoke(SearchArguments param) async {
+    final result = await _repository.search(param.searchText, pageNumber: param.pageNumber);
+  }
+}

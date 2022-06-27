f7b87c0c1833672297f4249c7543d9053f8ae76a
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-27 06:58:55 +0300

    Add search view model is created

--- lib/infra/core/modules/view_model_modules.dart
+++ lib/infra/core/modules/view_model_modules.dart
@@ -1,8 +1,11 @@
+import 'package:delivery_hero_flutter_study_case/presentation/search/search_view_model.dart';
 import 'package:delivery_hero_flutter_study_case/presentation/splash/splash_view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> viewModelModule() async {
+  searchViewModel = Provider.autoDispose((ref) => SearchViewModel(ref));
   splashViewModel = Provider.autoDispose((ref) => SplashViewModel(ref));
 }
 
+late final AutoDisposeProvider<SearchViewModel> searchViewModel;
 late final AutoDisposeProvider<SplashViewModel> splashViewModel;

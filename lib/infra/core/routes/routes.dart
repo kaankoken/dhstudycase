3d7ead4ca75aabe656cc0fd71484013c4e1d3ba0
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-27 06:57:58 +0300

    Fix route name correction & addition to main route

--- lib/infra/core/routes/routes.dart
+++ lib/infra/core/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_hero_flutter_study_case/domain/search/search_route.dart';
 import 'package:delivery_hero_flutter_study_case/domain/splash/splash_route.dart';
 import 'package:delivery_hero_flutter_study_case/infra/core/routes/app_routes.dart';
 import 'package:go_router/go_router.dart';
@@ -7,6 +8,7 @@ class Routes {
         initialLocation: AppRoutes.SPLASH,
         routes: [
           SplashRoute().route,
+          SearchRoute().route,
         ],
       );
 }

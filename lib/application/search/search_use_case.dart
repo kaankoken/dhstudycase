f3c51ea09bcd937af5c80c212a222d6e9cd0c6f4
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-27 10:26:42 +0300

    Update search usecase completed

--- lib/application/search/search_use_case.dart
+++ lib/application/search/search_use_case.dart
@@ -1,8 +1,11 @@
 import 'package:delivery_hero_flutter_study_case/application/search/interfaces/i_search_use_case.dart';
+import 'package:delivery_hero_flutter_study_case/domain/core/no_params.dart';
 import 'package:delivery_hero_flutter_study_case/domain/search/i_search_repository.dart';
+import 'package:delivery_hero_flutter_study_case/domain/search/models/dto_to_model.dart';
+import 'package:delivery_hero_flutter_study_case/domain/search/models/search_model.dart';
 import 'package:delivery_hero_flutter_study_case/infra/core/modules/repository_modules.dart';
+import 'package:delivery_hero_flutter_study_case/infra/core/utils/toast.dart';
 import 'package:delivery_hero_flutter_study_case/presentation/core/ui_state.dart';
-import 'package:delivery_hero_flutter_study_case/domain/search/search_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SearchUseCase implements ISearchUseCase {
@@ -11,7 +14,16 @@ class SearchUseCase implements ISearchUseCase {
   SearchUseCase(Ref ref) : _repository = ref.read(searchRepository);
 
   @override
-  Future<UIState<String, SearchModel>> invoke(SearchArguments param) async {
+  Future<UIState<NoParams, SearchModel>> invoke(SearchArguments param) async {
     final result = await _repository.search(param.searchText, pageNumber: param.pageNumber);
+
+    return result.when(
+      left: (left) {
+        ToastMessage.toast(left);
+
+        return const UIState.error(NoParams());
+      },
+      right: (right) => UIState.success(right.model),
+    );
   }
 }

dee8fe3a13e0ccc5efb7ef511be4f607445694ae
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-27 10:26:24 +0300

    Fix search model corrected according to search dto

--- lib/domain/search/models/search_model.dart
+++ lib/domain/search/models/search_model.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_hero_flutter_study_case/domain/search/models/search_item_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'search_model.freezed.dart';
@@ -5,10 +6,9 @@ part 'search_model.freezed.dart';
 @Freezed()
 class SearchModel with _$SearchModel {
   const factory SearchModel({
-    String? backdropImage,
-    @Default(100) int id,
-    String? posterImage,
-    @Default('') String title,
-    @Default(0.0) double vote,
+    int? page,
+    int? totalResults,
+    int? totalPages,
+    List<SearchItemModel>? items,
   }) = _SearchModel;
 }

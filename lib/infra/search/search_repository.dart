271d9c0b2972630b9d20549c91b851e394ee8c53
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-27 10:05:35 +0300

    Add search repository is created with repository module

---
+++ lib/infra/search/search_repository.dart
@@ -0,0 +1,30 @@
+import 'package:delivery_hero_flutter_study_case/domain/search/i_search_repository.dart';
+import 'package:delivery_hero_flutter_study_case/infra/core/api/i_api_service.dart';
+import 'package:delivery_hero_flutter_study_case/infra/core/api/network_manager.dart';
+import 'package:delivery_hero_flutter_study_case/infra/core/modules/service_modules.dart';
+import 'package:delivery_hero_flutter_study_case/infra/search/dto/search_dto.dart';
+import 'package:delivery_hero_flutter_study_case/infra/core/utils/either.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SearchRepository implements ISearchRepository {
+  late final IApiService _apiService;
+
+  SearchRepository(Ref ref) : _apiService = ref.read(apiService);
+  // TODO: check object to json for failure case
+  @override
+  Future<Either<String, SearchDto>> search(String searchText, {int? pageNumber}) async {
+    try {
+      final query = NetworkManager.baseQueryString;
+      query.putIfAbsent('include_adult', () => false);
+      query.putIfAbsent('page', () => pageNumber ?? 1);
+      query.putIfAbsent('query', () => searchText);
+
+      final result = await _apiService.search(query);
+
+      return Either.right(result);
+    } catch (e) {
+      return const Either.left('');
+      // Report to firebase etc.
+    }
+  }
+}

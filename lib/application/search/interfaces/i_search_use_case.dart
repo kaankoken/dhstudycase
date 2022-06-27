1dc583284876869cd4b79067e6b08e6169fef7af
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-27 10:27:23 +0300

    Add noparams model added

--- lib/application/search/interfaces/i_search_use_case.dart
+++ lib/application/search/interfaces/i_search_use_case.dart
@@ -1,5 +1,6 @@
 import 'package:delivery_hero_flutter_study_case/application/i_use_case.dart';
-import 'package:delivery_hero_flutter_study_case/domain/search/search_model.dart';
+import 'package:delivery_hero_flutter_study_case/domain/core/no_params.dart';
+import 'package:delivery_hero_flutter_study_case/domain/search/models/search_model.dart';
 import 'package:delivery_hero_flutter_study_case/presentation/core/ui_state.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -13,4 +14,4 @@ class SearchArguments with _$SearchArguments {
   }) = _SearchArguments;
 }
 
-abstract class ISearchUseCase extends IUseCase<UIState<String, SearchModel>, SearchArguments> {}
+abstract class ISearchUseCase extends IUseCase<UIState<NoParams, SearchModel>, SearchArguments> {}

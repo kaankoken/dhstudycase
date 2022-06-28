1a96534838929643e4073bf1acb2dc6aa79ca9e2
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-06-28 07:14:57 +0300

    Update app strings & colors updated

--- lib/infra/core/constants/app_colors.dart
+++ lib/infra/core/constants/app_colors.dart
@@ -3,7 +3,7 @@
 import 'package:delivery_hero_flutter_study_case/infra/core/extensions/hex_to_flutter_color.dart';
 
 class AppColors {
-  static final BLACK = "#ffffff".toColor();
+  static final BLACK = "#000000".toColor();
   static final LIGHT_BLACK = "#222222".toColor();
   static final LIGHT_GREY = "#d3d3d3".toColor();
   static final RED = '#ff0000'.toColor();
